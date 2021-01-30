8a1d9178741b0d782cdfd85e8cf91845eb3d18ab
kashaudhan <kashaudhan94@gmail.com>
2021-01-30 19:03:56 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import SingleColor from "./Singlecolor";
+import Values from "values.js";
 
 function App() {
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values("#00FF00").all(10));
+  console.log("List ", list);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+    } catch (err) {
+      setError(true);
+      console.log(err);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            className={`${error ? "error" : null}`}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#00FF00"
+          />
+          <button type="submit" className="btn">
+            Submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        {list.map((clr, index) => {
+          return <SingleColor key={index} {...clr} index={index} />;
+        })}
+      </section>
+    </>
   );
 }
 
